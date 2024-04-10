27a23115ed3228bc449b201d5736cce91825b52c
leahfeiner <lfeiner02@gmail.com>
2024-04-10 15:53:02 -0400

    fixed chckstyle errors

--- src/test/java/feiner/dictionary/EnglishDictionaryTest.java
+++ src/test/java/feiner/dictionary/EnglishDictionaryTest.java
@@ -20,14 +20,14 @@ class EnglishDictionaryTest {
         List<String> definitions = dictionary.getDefinition("bee");
 
         // then
-        List<String> expDef = Arrays.asList("p. p. of Be; -- used for been.", "An insect of " +
-                "the order Hymenoptera, and family Apidae (the honeybees), or " +
-                        "family Andrenidae (the solitary bees.) See Honeybee.",
-                "A neighborly gathering of people who engage in united labor for the benefit of an " +
-                        "individual or family; as, a quilting bee; a husking bee; " +
-                        "a raising bee.",
-                "Pieces of hard wood bolted to the sides of the bowsprit, to reeve the fore-topm" +
-                        "ast stays through; -- called also bee blocks.");
+        List<String> expDef = Arrays.asList("p. p. of Be; -- used for been.", "An insect of "
+                        + "the order Hymenoptera, and family Apidae (the honeybees), or "
+                        + "family Andrenidae (the solitary bees.) See Honeybee.",
+                "A neighborly gathering of people who engage in united labor for the benefit of an "
+                        + "individual or family; as, a quilting bee; a husking bee; "
+                        + "a raising bee.",
+                "Pieces of hard wood bolted to the sides of the bowsprit, to reeve the fore-topm"
+                        + "ast stays through; -- called also bee blocks.");
         assertNotNull(definitions, "The definitions list should not be null.");
         assertFalse(definitions.isEmpty(), "The definitions list shouldn't be empty for known word");
         assertEquals(expDef, definitions);
